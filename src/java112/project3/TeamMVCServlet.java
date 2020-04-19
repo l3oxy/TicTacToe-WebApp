e4178dcbc6ac5bf8a07e8aeaed0aaf00eb22b5c6
Kevin Leader <kleader@madisoncollege.edu>
2020-04-18 23:26:06 -0500

    added else to make it not switch turn if the spot is taken

--- src/java112/project3/TeamMVCServlet.java
+++ src/java112/project3/TeamMVCServlet.java
@@ -70,6 +70,8 @@ public class TeamMVCServlet extends HttpServlet {
                 } else {
                     boardStates.set(cellSelected, X);
                 }
+            } else {
+                player1Turn = !player1Turn;
             }
         }
 
